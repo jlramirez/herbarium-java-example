463f0265aeb0d27b90f262ec66a5a54b398d2c89
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2015-11-18 12:58:48 -0800

    Code Improvements

--- src/main/java/tech/sirwellington/alchemy/example/http/BotanicalGarden.java
+++ src/main/java/tech/sirwellington/alchemy/example/http/BotanicalGarden.java
@@ -6,17 +6,8 @@
 
 package tech.sirwellington.alchemy.example.http;
 
-import com.google.common.base.Splitter;
-import com.google.common.collect.Lists;
-import com.google.gson.JsonElement;
-import java.util.ArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import tech.sirwellington.alchemy.http.AlchemyHttp;
-import tech.sirwellington.alchemy.http.HttpResponse;
-
-import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
-import static tech.sirwellington.alchemy.generator.StringGenerators.stringsFromFixedList;
 
 /**
  *
@@ -29,31 +20,7 @@ public class BotanicalGarden
 
     public static void main(String[] args) throws Exception
     {
-        queryHerbariumByState();
+        Herbarium.queryHerbariumByState();
     }
 
-    private static void queryHerbariumByState() throws Exception
-    {
-        Iterable<String> states = Splitter.on(", ")
-            .omitEmptyStrings()
-            .split("NY, NJ, CA, MD, VA, DC, FL, TX, MN");
-
-        ArrayList<String> statesList = Lists.newArrayList(states);
-        String state = one(stringsFromFixedList(statesList));
-
-        String url = "http://sweetgum.nybg.org/science/v1/institution/" + state;
-
-        AlchemyHttp http = AlchemyHttp.newDefaultInstance();
-
-        LOG.info("Making GET request to {}", url);
-
-        HttpResponse response = http.go()
-            .get()
-            .at(url);
-
-        LOG.info("Response: {} : {}", response.statusCode(), response.responseHeaders());
-
-        JsonElement body = response.body();
-        LOG.info("Response Body: {}", body);
-    }
 }
---
+++ src/main/java/tech/sirwellington/alchemy/example/http/Herbarium.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tech.sirwellington.alchemy.example.http;
+
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+import com.google.gson.JsonElement;
+import java.util.ArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.sirwellington.alchemy.http.AlchemyHttp;
+import tech.sirwellington.alchemy.http.HttpResponse;
+
+import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
+import static tech.sirwellington.alchemy.generator.StringGenerators.stringsFromFixedList;
+
+/**
+ *
+ * @author SirWellington
+ */
+public class Herbarium
+{
+
+    private final static Logger LOG = LoggerFactory.getLogger(Herbarium.class);
+
+    public static void queryHerbariumByState() throws Exception
+    {
+
+        String state = selectRandomState();
+
+        String url = "http://sweetgum.nybg.org/science/v1/institution/" + state;
+
+        AlchemyHttp http = AlchemyHttp.newDefaultInstance();
+
+        LOG.info("Making GET request to {}", url);
+
+        HttpResponse response = http.go()
+            .get()
+            .at(url);
+
+        LOG.info("Response: {} : {}", response.statusCode(), response.responseHeaders());
+
+        JsonElement body = response.body();
+        LOG.info("Response Body: {}", body);
+    }
+
+    private static String selectRandomState()
+    {
+        Iterable<String> states = Splitter.on(", ")
+            .omitEmptyStrings()
+            .split("NY, NJ, CA, MD, VA, DC, FL, TX, MN");
+
+        ArrayList<String> statesList = Lists.newArrayList(states);
+        return one(stringsFromFixedList(statesList));
+    }
+
+}
